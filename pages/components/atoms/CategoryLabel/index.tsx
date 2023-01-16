a046fa43047a0cf21cb67eb543ee843bc918c665
김혜원 <dev.blair06@gmail.com>
2023-01-16 20:09:34 +0900

    feat:#4 상단 navbar 구현

--- pages/components/atoms/CategoryLabel/index.tsx
+++ pages/components/atoms/CategoryLabel/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CategoryLabel = ({ children }: { children: string }) => {
   return (
-    <div className="w-80 border-b-4 border-b-primary">
+    <div className=" border-b-4 border-b-primary w-64">
       <p className="text-4xl leading-6 text-right font-semibold">{children}</p>
     </div>
   );
--- pages/components/templates/index.tsx
+++ pages/components/templates/index.tsx
@@ -4,6 +4,15 @@ import React from "react";
 const BaseTemplate = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex">
+      <nav className="bg-primary w-96 h-11 absolute right-0 ">
+        <ul className="h-full flex justify-around items-center text-white">
+          <li>Problems</li>
+          <li>history</li>
+          <li>Rank</li>
+          <li>|</li>
+          <li>Members</li>
+        </ul>
+      </nav>
       <ProfileArea></ProfileArea>
       {children}
     </div>
